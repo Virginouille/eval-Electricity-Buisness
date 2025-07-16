6ba2f62eaedff1305ecf7d982845dc9da74ab75d
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-07-16 09:29:14 +0200

    initial commit

---
+++ modules/Borne.js
@@ -0,0 +1,15 @@
+export class Borne {
+
+    idBorne;
+    type;
+    date;
+    heure;
+
+
+    constructor(idBorne, type, date, heure) {
+        this.idBorne;
+        this.type;
+        this.date;
+        this.heure;
+    }
+}
